2592744b5a92e77e3ea2401fa22380eb8b7980cc
odelola <integralandintegrations@gmail.com>
2024-06-10 14:35:48 +0100

    Added Loaders, changed styles, added overlay and change pagination in Swiper

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,37 @@
 import { useEffect } from 'react'
+import Router from "next/router"
+import Head from 'next/head'
 import AOS from 'aos'
 import "aos/dist//aos.css"
 import '../styles/globals.css'
+import '../styles/NProgress.css'
+import '../styles/Swiper.css'
 import '../styles/Footer.css'
 import '../styles/Contact.css'
-import '../styles/Swiper.css'
 import Layout from '../src/components/Layout'
 import Lenis from '@studio-freight/lenis'
+import NProgress from "nprogress"
 
 
 function MyApp({ Component, pageProps }) {
+
+
+  NProgress.configure({ showSpinner: false })
+
+  Router.events.on('routeChangeStart', () => {
+    NProgress.start()
+  })
+
+  Router.events.on('routeChangeComplete', () => {
+    NProgress.done()
+  })
+
   useEffect(() => {
-
-    const lenis = new Lenis({lerp: .8, duration: 3})
-
-
-
+    const lenis = new Lenis({ lerp: .8, duration: 3 })
     const raf = (time) => {
-
       lenis.raf(time)
-
       requestAnimationFrame(raf)
-
     }
-
-
-
     requestAnimationFrame(raf)
 
     AOS.init();
@@ -35,9 +41,16 @@ function MyApp({ Component, pageProps }) {
 
   }, [])
 
-  return <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://unpkg.com/nprogress@0.2.0/nprogress.css" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
 
 export default MyApp
