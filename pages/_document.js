d5fcb96547afd0972cf82c43d5adc83fff780c47
odelola <integralandintegrations@gmail.com>
2024-07-09 14:09:39 +0100

    Final Update I pray

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300@500&display=swap" rel="stylesheet" />
                 <link rel="stylesheet" href="https://unpkg.com/nprogress@0.2.0/nprogress.css" />
             </Head>
             <body>
